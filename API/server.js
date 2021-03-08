3fb3e43379bb84161ee3b88044acf53ecef6e42b
Wanderson Knight <39245594+WandersonKnight@users.noreply.github.com>
2021-03-08 16:52:39 -0300

    Add files via upload

---
+++ API/server.js
@@ -0,0 +1,8 @@
+const http = require("http");
+const app = require("./app");
+const fs = require("fs");
+
+const port = process.env.PORT || 3000;
+const server = http.createServer(app);
+
+server.listen(port);
